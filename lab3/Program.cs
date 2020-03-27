4c0ac9f3df02bfe3b99c5686e086ba3ad96e8a2d
mufteev <57632588+mufteev@users.noreply.github.com>
2020-03-28 01:02:25 +0500

    Lab 3

---
+++ lab3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    class Program
+    {
+        static Dictionary<string, string> assocSection = new Dictionary<string, string>
+        {
+            { "0", "(pdf|download|help|gost|ref)" },
+            { "1", "pdf" },
+            { "2", "download" },
+            { "3", "help" },
+            { "4", "gost" },
+            { "5", "ref" }
+        };
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите раздел, из которого следует скачать файлы");
+            Console.Write("0 - Все разделы" +
+                        "\n1 - pdf" +
+                        "\n2 - download" +
+                        "\n3 - help" +
+                        "\n4 - gost" +
+                        "\n5 - ref" +
+                        "\n> ");
+            var section = Console.ReadLine();
+
+            var data = WebHelp.GetHtmlData("https://pcoding.ru/darkNet.php");
+            var links = WebHelp.GetSection(data, assocSection[section]);
+
+            Console.Write("Укажите директорию в которую выгрузить файлы (. - текущая директория)\n(Можно указать несуществующую директорию)\n> ");
+            var path = Console.ReadLine();
+            path = Path.GetFullPath(path);
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            WebHelp.SaveFilesWithStats(links, path);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab3/WebHelp.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_3
+{
+    public static class WebHelp
+    {
+        public static string GetHtmlData(string link)
+        {
+            string data;
+
+            using (var wc = new WebClient())
+            {
+                data = Encoding.UTF8.GetString(wc.DownloadData(link));
+            }
+            return data;
+        }
+
+        public static List<string> GetSection(string data, string section)
+            => new Regex($@"href=(?<link>https:\/\/pcoding\.ru\/{section}\/[^\s]*)", RegexOptions.IgnoreCase)
+            .Matches(data)
+            .Cast<Match>()
+            .Select(x => x.Groups["link"].Value)
+            .ToList();
+
+        public static void SaveFilesWithStats(List<string> links, string pathDir)
+        {
+            Console.WriteLine();
+            var downloader = new WebDownloadFilesOnDir(links, pathDir);
+            downloader.Download();
+        }
+    }
+
+    internal class WebDownloadFilesOnDir
+    {
+        private List<string> _queue;
+        private string _dir;
+        private int _count;
+        private string _currentFile;
+        private bool _isRun;
+
+        private string _current;
+
+        public WebDownloadFilesOnDir(List<string> links, string pathDir)
+        {
+            _queue = links;
+            _count = links.Count;
+            _dir = pathDir;
+        }
+
+        private void _webClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            var current = e.BytesReceived;
+            var total = e.TotalBytesToReceive;
+            var percent = (double)current / total * 5;
+            Console.Write($"\r({_current}) {_currentFile} {new string('.', (int)percent)}");
+        }
+
+        private Task WaitForDownloadComplete(string link, string path)
+        {
+            using (var _webClient = new WebClient())
+            {                
+                _webClient.DownloadProgressChanged += _webClient_DownloadProgressChanged;
+                var task = _webClient.DownloadFileTaskAsync(link, path);
+                task.ContinueWith(_ =>
+                {
+                    _webClient.DownloadProgressChanged -= _webClient_DownloadProgressChanged;
+                }, TaskContinuationOptions.ExecuteSynchronously);
+                return task; 
+            }
+        }
+
+        public async void Download()
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                _isRun = false;
+                _current = $"{i + 1}/{_count}";
+                _currentFile = Path.GetFileName(_queue[i]);
+                var fullPath = Path.Combine(_dir, _currentFile);
+                await WaitForDownloadComplete(_queue[i], fullPath);
+                // Это треш, асинхронный метод скачивания работает как попало...
+                Thread.Sleep(10);
+                Console.WriteLine($"\r({_current}) {_currentFile}      ");
+            }
+        }
+    }
+}
