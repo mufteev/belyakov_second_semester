217e588717521c8674035ee14237f486bebf02ea
mufteev <57632588+mufteev@users.noreply.github.com>
2020-05-29 17:05:36 +0500

    Жадный алгоритм

--- lab_8/Form1.Designer.cs
+++ lab_8/Form1.Designer.cs
@@ -42,10 +42,14 @@ namespace lab_8
             this.metroButton2 = new MetroFramework.Controls.MetroButton();
             this.MenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.ItemCheckConnection = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConstrainsWay = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.metroSM)).BeginInit();
             this.metroPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGView)).BeginInit();
             this.MenuStrip.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // metroSM
@@ -54,10 +58,11 @@ namespace lab_8
             // 
             // MBtnGet
             // 
-            this.MBtnGet.Location = new System.Drawing.Point(31, 63);
-            this.MBtnGet.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MBtnGet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MBtnGet.Location = new System.Drawing.Point(2, 2);
+            this.MBtnGet.Margin = new System.Windows.Forms.Padding(2);
             this.MBtnGet.Name = "MBtnGet";
-            this.MBtnGet.Size = new System.Drawing.Size(175, 63);
+            this.MBtnGet.Size = new System.Drawing.Size(184, 45);
             this.MBtnGet.TabIndex = 0;
             this.MBtnGet.Text = "Получить данные";
             this.MBtnGet.Click += new System.EventHandler(this.MBtnGet_Click);
@@ -68,13 +73,14 @@ namespace lab_8
             this.metroPanel1.Controls.Add(this.metroRadioButton3);
             this.metroPanel1.Controls.Add(this.metroRadioButton2);
             this.metroPanel1.Controls.Add(this.metroRadioButton1);
+            this.metroPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.metroPanel1.HorizontalScrollbarBarColor = true;
             this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
             this.metroPanel1.HorizontalScrollbarSize = 8;
-            this.metroPanel1.Location = new System.Drawing.Point(31, 149);
-            this.metroPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroPanel1.Location = new System.Drawing.Point(2, 51);
+            this.metroPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.metroPanel1.Name = "metroPanel1";
-            this.metroPanel1.Size = new System.Drawing.Size(175, 146);
+            this.metroPanel1.Size = new System.Drawing.Size(184, 192);
             this.metroPanel1.TabIndex = 8;
             this.metroPanel1.VerticalScrollbarBarColor = true;
             this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
@@ -84,7 +90,7 @@ namespace lab_8
             // 
             this.metroRadioButton4.AutoSize = true;
             this.metroRadioButton4.Location = new System.Drawing.Point(34, 110);
-            this.metroRadioButton4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroRadioButton4.Margin = new System.Windows.Forms.Padding(2);
             this.metroRadioButton4.Name = "metroRadioButton4";
             this.metroRadioButton4.Size = new System.Drawing.Size(79, 15);
             this.metroRadioButton4.TabIndex = 11;
@@ -95,7 +101,7 @@ namespace lab_8
             // 
             this.metroRadioButton3.AutoSize = true;
             this.metroRadioButton3.Location = new System.Drawing.Point(34, 80);
-            this.metroRadioButton3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroRadioButton3.Margin = new System.Windows.Forms.Padding(2);
             this.metroRadioButton3.Name = "metroRadioButton3";
             this.metroRadioButton3.Size = new System.Drawing.Size(74, 15);
             this.metroRadioButton3.TabIndex = 10;
@@ -106,7 +112,7 @@ namespace lab_8
             // 
             this.metroRadioButton2.AutoSize = true;
             this.metroRadioButton2.Location = new System.Drawing.Point(34, 50);
-            this.metroRadioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroRadioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.metroRadioButton2.Name = "metroRadioButton2";
             this.metroRadioButton2.Size = new System.Drawing.Size(116, 15);
             this.metroRadioButton2.TabIndex = 9;
@@ -118,7 +124,7 @@ namespace lab_8
             this.metroRadioButton1.AutoSize = true;
             this.metroRadioButton1.Checked = true;
             this.metroRadioButton1.Location = new System.Drawing.Point(34, 20);
-            this.metroRadioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroRadioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.metroRadioButton1.Name = "metroRadioButton1";
             this.metroRadioButton1.Size = new System.Drawing.Size(125, 15);
             this.metroRadioButton1.TabIndex = 8;
@@ -131,24 +137,28 @@ namespace lab_8
             this.DGView.AllowUserToAddRows = false;
             this.DGView.AllowUserToDeleteRows = false;
             this.DGView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DGView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.DGView.Location = new System.Drawing.Point(220, 63);
-            this.DGView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DGView.Location = new System.Drawing.Point(190, 2);
+            this.DGView.Margin = new System.Windows.Forms.Padding(2);
             this.DGView.Name = "DGView";
             this.DGView.RowHeadersVisible = false;
             this.DGView.RowHeadersWidth = 51;
+            this.tableLayoutPanel1.SetRowSpan(this.DGView, 5);
             this.DGView.RowTemplate.Height = 24;
-            this.DGView.Size = new System.Drawing.Size(397, 328);
+            this.DGView.Size = new System.Drawing.Size(412, 330);
             this.DGView.TabIndex = 9;
             // 
             // metroButton2
             // 
-            this.metroButton2.Location = new System.Drawing.Point(31, 308);
-            this.metroButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.metroButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroButton2.Location = new System.Drawing.Point(2, 247);
+            this.metroButton2.Margin = new System.Windows.Forms.Padding(2);
             this.metroButton2.Name = "metroButton2";
-            this.metroButton2.Size = new System.Drawing.Size(175, 83);
+            this.metroButton2.Size = new System.Drawing.Size(184, 45);
             this.metroButton2.TabIndex = 10;
             this.metroButton2.Text = "Найти решение";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
             // 
             // MenuStrip
             // 
@@ -156,7 +166,7 @@ namespace lab_8
             this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ItemCheckConnection});
             this.MenuStrip.Name = "MenuStrip";
-            this.MenuStrip.Size = new System.Drawing.Size(245, 48);
+            this.MenuStrip.Size = new System.Drawing.Size(245, 26);
             // 
             // ItemCheckConnection
             // 
@@ -166,19 +176,56 @@ namespace lab_8
             this.ItemCheckConnection.Text = "Проверить соединение";
             this.ItemCheckConnection.Click += new System.EventHandler(this.ItemCheckConnection_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.12583F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.87417F));
+            this.tableLayoutPanel1.Controls.Add(this.MBtnGet, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.metroPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.DGView, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.metroButton2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.txtConstrainsWay, 0, 4);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 60);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.13652F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.86348F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(604, 334);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ограничение на длину пути:";
+            // 
+            // txtConstrainsWay
+            // 
+            this.txtConstrainsWay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConstrainsWay.Location = new System.Drawing.Point(3, 310);
+            this.txtConstrainsWay.Name = "txtConstrainsWay";
+            this.txtConstrainsWay.Size = new System.Drawing.Size(182, 20);
+            this.txtConstrainsWay.TabIndex = 12;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(634, 410);
             this.ContextMenuStrip = this.MenuStrip;
-            this.Controls.Add(this.metroButton2);
-            this.Controls.Add(this.DGView);
-            this.Controls.Add(this.metroPanel1);
-            this.Controls.Add(this.MBtnGet);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Padding = new System.Windows.Forms.Padding(15, 49, 15, 16);
+            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
             this.Resizable = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Style = MetroFramework.MetroColorStyle.Orange;
@@ -188,6 +235,8 @@ namespace lab_8
             this.metroPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DGView)).EndInit();
             this.MenuStrip.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -205,6 +254,9 @@ namespace lab_8
         private MetroFramework.Controls.MetroButton metroButton2;
         private System.Windows.Forms.ContextMenuStrip MenuStrip;
         private System.Windows.Forms.ToolStripMenuItem ItemCheckConnection;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConstrainsWay;
     }
 }
 
--- lab_8/Form1.cs
+++ lab_8/Form1.cs
@@ -61,6 +61,61 @@ namespace lab_8
 
             return dt;
         }
+
+        async Task<DataTable> GetDataOptimization(int wayConstraint)
+        {
+            var dt = new DataTable();
+
+            try
+            {
+                msc.Open();
+                var query = @"SELECT cities.name_city          AS 'City',
+                                     prices.name_shop          AS 'Shop',
+                                     prices.price              AS 'Price',
+                                     cities.way                AS 'Way',
+                                     prices.price / cities.way AS 'Relative'
+                              FROM (cities INNER JOIN merge ON cities.id_city = merge.id_city)
+                                       INNER JOIN prices ON prices.id_shop = merge.id_shop
+                              ORDER BY Relative DESC";
+                var cmd = msc.CreateCommand();
+                cmd.CommandText = query;
+
+                var reader = await cmd.ExecuteReaderAsync();
+
+                dt.Columns.Add("City");
+                dt.Columns.Add("Shop");
+                dt.Columns.Add("Price");
+                dt.Columns.Add("Way");
+                dt.Columns.Add("Relative");
+
+                var totalWay = 0;
+                var totalPrice = 0M;
+
+                foreach (IDataRecord item in reader)
+                {
+                    if (wayConstraint > 0 && totalWay + item.GetInt32(3) > wayConstraint) break;
+                    totalWay += item.GetInt32(3);
+                    totalPrice += item.GetDecimal(2);
+                    dt.Rows.Add(item.GetValue(0),
+                        item.GetValue(1),
+                        item.GetValue(2),
+                        item.GetValue(3),
+                        item.GetValue(4));
+                }
+
+                MessageBox.Show($"Sum Price: {totalPrice:C}\nFor sum Way: {totalWay} km", "Transport");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при получении данных:\n{ex.Message}", "Transport");
+            }
+            finally
+            {
+                msc.Close();
+            }
+
+            return dt;
+        }
         #endregion
 
         public Form1()
@@ -92,6 +147,19 @@ namespace lab_8
             catch (Exception ex)
             {
                 MessageBox.Show($"Не удалось подключится к удалённому ресурсу.\n\n{ex.Message}", "Transport", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private async void metroButton2_Click(object sender, EventArgs e)
+        {
+            if (metroRadioButton1.Checked == true)
+            {
+                var way = int.TryParse(txtConstrainsWay.Text, out int cnstr) ? cnstr : 0;
+                DGView.DataSource = await GetDataOptimization(way);
+            }
+            else
+            {
+                MessageBox.Show("Выбранный алгоритм не реализован", "Transport");
             }
         }
     }
